a1d636d3bc252b780926efb35440d8e25abf119e
Ben Rogers <benrogers@mac.com>
2021-03-28 07:06:43 -0400

    Remove tests now covered in batch

--- test/test_columnNumbers.js
+++ test/test_columnNumbers.js
@@ -5,13 +5,6 @@ const gsheet = require('../gsheet.js')
 describe('columnLetter', function() {
     const columnLetter = gsheet.columnLetter
 
-    it('should return A for index 1', function() {
-        columnLetter(1).should.equal('A')
-    })
-    it('should return Z for index 26', function() {
-        columnLetter(26).should.equal('Z')
-    })
-
     const letters = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ']
     letters.forEach((l, i) => {
         it('should return ' + l + ' for index ' + (i + 1), function() {
